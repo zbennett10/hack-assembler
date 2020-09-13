9c8f8dc2b79bc120456f4bd0ca502f7864f39eb4
Zachary Bennett <bennett.zachary@outlook.com>
2020-09-13 16:37:28 -0500

    finished parsing C Commands

--- src/lib/hack-parser.ts
+++ src/lib/hack-parser.ts
@@ -21,16 +21,18 @@ export class HackParser {
 
         // First impl w/o symbol table
         const binaryLines = lines.map(line => {
-            const commandType = this.getCommandType(line);
+            const cleanLine = line.replace(/\s/g, '');
+            const commandType = this.getCommandType(cleanLine);
             if (commandType === CommandType.NONE) {
                 return '\n';
             }
 
+            // TODO: need to parse L_COMMAND separately here...
             if (commandType === CommandType.A_COMMAND || commandType === CommandType.L_COMMAND) {
-                return this.getSymbol(line, commandType);
+                return this.getSymbol(cleanLine, commandType);
             }
 
-            return this.composeCCommandMnemonics(line);
+            return ('111' + this.composeCCommandMnemonics(cleanLine)).split('').map((b, i) => (i + 1) % 4 === 0 ? b + ' ' : b).join('');
         });
 
         return binaryLines.join('\n');
@@ -57,7 +59,7 @@ export class HackParser {
                 return this.fill(binary).split('').map((b, i) => (i + 1) % 4 === 0 ? b + ' ' : b).join('');
             }
             case CommandType.C_COMMAND: {
-                return this.fill(('111' + this.composeCCommandMnemonics(line))).split('').map((b, i) => (i + 1) % 4 === 0 ? b + ' ' : b).join('');
+                return ('111' + this.composeCCommandMnemonics(line)).split('').map((b, i) => (i + 1) % 4 === 0 ? b + ' ' : b).join('');
 
             }
             case CommandType.L_COMMAND: { // For parsing pseudo-commands like (LOOP) or (END)
@@ -71,13 +73,14 @@ export class HackParser {
     }
 
     private composeCCommandMnemonics(line: string): string {
-        return this.getDest(line) + this.getComp(line) + this.getJump(line);
+        return this.getComp(line) + this.getDest(line) + this.getJump(line);
     }
 
     private getDest(line: string): string {
         const equalsIdx = line.indexOf('=');
         if (equalsIdx > -1) {
-            switch (line.slice(0, equalsIdx)) {
+            const dest = line.slice(0, equalsIdx);
+            switch (dest) {
                 case 'M': return '001';
                 case 'D': return '010';
                 case 'MD': return '011';
@@ -85,21 +88,69 @@ export class HackParser {
                 case 'AM': return '101';
                 case 'AD': return '110';
                 case 'AMD': return '111';
-                default: return '';
+                default: {
+                    throw new Error(`Invalid dest field ==> ${dest}`);
+                };
             }
         }
-        return '';
+        return '000';
     }
 
-    // TODO: 9/11/2020 - left off here - see page 109 in book
     private getComp(line: string): string {
-        return line;
+        const equalitySignIdx = line.indexOf('=');
+        const semicolonIdx = line.indexOf(';');
+
+        let comp = '';
+        // check for just comp
+        if (equalitySignIdx < 0 && semicolonIdx < 0) {
+            comp = line;
+        } else if (equalitySignIdx > -1 && semicolonIdx < 0) { // check for dest=comp
+            comp = line.slice(equalitySignIdx + 1);
+        } else if (equalitySignIdx > -1 && semicolonIdx > -1) { // check for dest=comp;jump
+            comp = line.slice(equalitySignIdx + 1, semicolonIdx);
+        } else { // check for comp;jump
+            comp = line.slice(0, semicolonIdx);
+        }
+        
+        return this.getARegisterBitVal(comp) + this.determineCompBits(comp);
+    }
+
+    // Return c1-c6 of the 16-bit machine language instruction set for the Hack platform where:
+    private determineCompBits(line: string): string {
+        switch(true) {
+            case (line === '0'): return '101010';
+            case (line === '1'): return '111111';
+            case (line === '-1'): return '111010';
+            case (line === 'D'): return '001100';
+            case (line === 'A' || line === 'M'): return '110000';
+            case (line === '!D'): return '001101';
+            case (line === '!A' || line === '!M'): return '110001';
+            case (line === '-D'): return '001111';
+            case (line === '-A' || line === '-M'): return '110011';
+            case (line === 'D+1'): return '011111';
+            case (line === 'A+1' || line === 'M+1'): return '110111';
+            case (line === 'D-1'): return '001110';
+            case (line === 'A-1' || line === 'M-1'): return '110010';
+            case (line === 'D+A' || line === 'D+M'): return '000010';
+            case (line === 'D-A' || line === 'D-M'): return '010011';
+            case (line === 'A-D' || line === 'M-D'): return '000111';
+            case (line === 'D&A' || line === 'D&M'): return '000000';
+            case (line === 'D|A' || line === 'D|M'): return '010101';
+            default: {
+                throw new Error(`Unable to parse comp field -> Invalid comp present!  =>> ${line}` )
+            }
+        }
+    }
+
+    private getARegisterBitVal(comp: string): string {
+        return comp.includes('M') ? '1' : '0';
     }
 
     private getJump(line: string): string {
         const semicolonIdx = line.indexOf(';');
         if (semicolonIdx > -1) {
-            switch (line.slice(semicolonIdx)) {
+            const jump = line.slice(semicolonIdx + 1);
+            switch (jump) {
                 case 'JGT': return '001';
                 case 'JEQ': return '010';
                 case 'JGE': return '011';
@@ -107,10 +158,12 @@ export class HackParser {
                 case 'JNE': return '101';
                 case 'JLE': return '110';
                 case 'JMP': return '111';
-                default: return '';
+                default: {
+                    throw new Error(`Invalid jump field ==> ${jump}`);
+                };
             }
         }
-        return '';
+        return '000';
     }
 
     private fill(binary: string): string {
